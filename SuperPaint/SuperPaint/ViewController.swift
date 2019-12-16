a9727e343062e21eaf71b1cd2a41b834a97d4242
MED1D <medvedev.id@bk.ru>
2019-12-16 20:42:49 +0300

    (ED-1) Создал пустой проект

---
+++ SuperPaint/SuperPaint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperPaint
+//
+//  Created by Иван Медведев on 16/12/2019.
+//  Copyright © 2019 Fixiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
