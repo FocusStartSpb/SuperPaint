1097a5e497dd477a5c9b311baba83fb9636612be
Ivan <32410337+Med1D@users.noreply.github.com>
2019-12-18 20:57:49 +0300

    (ED-6) Добавил модуль главного экрана и пустой репозиторий (#12)
    * (ED-6) Добавил модуль главного экрана и пустой репозиторий
    
    * (ED-6) Пофиксил ошибки swiftlint
    
    * (ED-6) Поправил замечания после codereview
    
    * (ED-6) Поправил замечания после codereview[2]

---
+++ SuperPaint/SuperPaint/ImagesCollectionModule/Protocols/IImagesCollectionPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  IImagesCollectionPresenter.swift
+//  SuperPaint
+//
+//  Created by Иван Медведев on 18/12/2019.
+//  Copyright © 2019 Fixiki. All rights reserved.
+//
+
+import Foundation
+
+protocol IImagesCollectionPresenter: AnyObject
+{
+}
---
+++ SuperPaint/SuperPaint/Repository/Repository.swift
@@ -0,0 +1,13 @@
+//
+//  Repository.swift
+//  SuperPaint
+//
+//  Created by Иван Медведев on 18/12/2019.
+//  Copyright © 2019 Fixiki. All rights reserved.
+//
+
+import Foundation
+
+final class Repository
+{
+}
