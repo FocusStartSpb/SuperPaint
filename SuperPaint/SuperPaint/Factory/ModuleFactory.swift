4a69a1fd40ba85fdf1a5071841bd09b54c7c4280
Stanislav Kozlov <bleymy@mail.ru>
2019-12-18 21:34:11 +0300

    (ED-7) Добавил фабрику (#13)
    * (ED-7) Создал Factory
    
    * (ED-7) Добавил xcuserstate в гитигнор
    
    * (ED-7) Убрал создание репозитория в фабрику модулей, сделал репозиторий приватным

---
+++ SuperPaint/SuperPaint/Factory/ModuleFactory.swift
@@ -0,0 +1,33 @@
+//
+//  ModuleFactory.swift
+//  SuperPaint
+//
+//  Created by Stanislav on 18/12/2019.
+//  Copyright © 2019 Fixiki. All rights reserved.
+//
+
+import UIKit
+
+struct ModuleFactory
+{
+	private let repository: IRepository
+
+	init() {
+		self.repository = Repository()
+	}
+
+	func createNavigationController() -> UIViewController {
+		let imagesCollectionModule = createImagesCollectionModule()
+		let navigationController = UINavigationController(rootViewController: imagesCollectionModule)
+		return  navigationController
+	}
+
+	func createImagesCollectionModule() -> UIViewController {
+		let router = ImagesCollectionRouter()
+		let presenter = ImagesCollectionPresenter(router: router, repository: repository)
+		let view = ImagesCollectionViewController(presenter: presenter)
+		presenter.inject(view: view)
+		router.inject(view: view)
+		return view
+	}
+}
--- SuperPaint/SuperPaint/Repository/Repository.swift
+++ SuperPaint/SuperPaint/Repository/Repository.swift
@@ -11,3 +11,7 @@ import Foundation
 final class Repository
 {
 }
+
+extension Repository: IRepository
+{
+}
